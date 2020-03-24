91983ac2a6c0b398ab8c25a72817f0763594ae44
ikhans <irfan.khan-s@capgemini.com>
2020-03-24 12:23:06 +0530

    Removed unused dependency and reformatted the code

--- app/src/main/java/com/irfan/singapore/ui/home/HomeFragment.java
+++ app/src/main/java/com/irfan/singapore/ui/home/HomeFragment.java
@@ -48,15 +48,17 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
-
+        // Initialize View Model
         initializeViewModel();
 
+        // Adding Observers to View Model
         addObservers();
 
         return rootView;
     }
 
     private void addObservers() {
+        // get response from the api
         homeViewModel
                 .getResponseData()
                 .observe(getViewLifecycleOwner(), new Observer<PsiResponse>() {
@@ -68,6 +70,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
                     }
                 });
 
+        // handle on response error
         homeViewModel
                 .getErrorMessage()
                 .observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -77,6 +80,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
                     }
                 });
 
+        // dialog show and dismiss, on App load and after API response
         homeViewModel
                 .getProgressDialog()
                 .observe(getViewLifecycleOwner(), new Observer<Boolean>() {
@@ -96,14 +100,17 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
     public void onMapReady(GoogleMap map) {
         googleMap = map;
 
+        // get response data and add Markers to it
         for (int i = 0; i < psiResponse.getRegionMetadata().size(); i++) {
             if (!psiResponse.getRegionMetadata().get(i).getName().equals(getActivity().getResources().getString(R.string.national_string))) {
                 drawMarkers(psiResponse.getRegionMetadata().get(i));
             }
         }
-        LatLng latLngTest = new LatLng(psiResponse.getRegionMetadata().get(0).getLabelLocation().getLatitude(),
-                psiResponse.getRegionMetadata().get(0).getLabelLocation().getLongitude());
 
+        // Get first Latitude & Longitude of response
+        LatLng latLngTest = new LatLng(
+                psiResponse.getRegionMetadata().get(0).getLabelLocation().getLatitude(),
+                psiResponse.getRegionMetadata().get(0).getLabelLocation().getLongitude());
 
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngTest, 10f));
         googleMap.setOnInfoWindowClickListener(this);
@@ -118,6 +125,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
     @Override
     public void onInfoWindowClick(Marker marker) {
+        // Add a dialog window on click of Marker
         Dialog readingsdialog = new Dialog(getActivity());
         readingsdialog.setContentView(R.layout.dialog_reading_details);
         readingsdialog.setCancelable(true);
@@ -125,15 +133,18 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
         TextView readingsTitle = readingsdialog.findViewById(R.id.readingsTitle);
         RecyclerView recyclerViewReadings = readingsdialog.findViewById(R.id.recyclerViewReadings);
 
+        // Set a recyclerview in dialog
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         recyclerViewReadings.setLayoutManager(mLayoutManager);
 
+        // Add divider between items in recyclerview
         CustomItemDecoration customItemDecoration = new CustomItemDecoration(getActivity(),
                 R.dimen.margin4);
         recyclerViewReadings.addItemDecoration(customItemDecoration);
 
         readingsTitle.setText(ReadingListDetails.getRegionTitle(marker.getTitle()).toUpperCase() + " Readings");
 
+        // Set adapter in recyclerview
         ReadingItemsAdapter homePageAdapter = new ReadingItemsAdapter(getActivity(), ReadingListDetails.getReadingDetails(
                 readings,
                 ReadingListDetails.getRegionTitle(marker.getTitle())));
@@ -144,6 +155,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, Google
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void displayProgressDialog(boolean display) {
+        // display loading dialog on App Start
         if (dialog == null) {
             dialog = new Dialog(getActivity());
             dialog.setContentView(R.layout.loading_message);
